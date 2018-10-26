d2f5dedcca17b7ef579ac1fb44e424216d2d6647
Piotr Palarz <piotr@depalage.com>
2018-10-26 13:34:09 +0200

    64-finish

--- 64/js/scripts.js
+++ 64/js/scripts.js
@@ -4,3 +4,17 @@ var btn2 = document.querySelector("#btn2"),
 function sayHello() {
     alert("Hej!");
 }
+
+btn2.onclick = sayHello;
+btn2.onclick = function() {
+    alert("Witaj!");
+};
+
+btn3.addEventListener("click", sayHello, false);
+btn3.addEventListener("click", function() {
+    alert("Witaj!");
+}, false);
+
+btn2.onclick = null;
+
+btn3.removeEventListener("click", sayHello, false);
