a01b623cfa610e191b50c52d8198868e96fa8f7b
Piotr Palarz <piotr@depalage.com>
2018-10-26 13:50:45 +0200

    81-finish

--- 81/js/scripts.js
+++ 81/js/scripts.js
@@ -7,4 +7,13 @@ function getValues() {
 
     var id = 12331;
 
+    return [obj, id];
+
 }
+
+var values = getValues(),
+    data = values[0],
+    id = values[1];
+
+console.log(data);
+console.log(id);
